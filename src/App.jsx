c2bfafb9e6ef72f49968fdb8f33d3f2cb9c03b91
Dhaya3132 <dhayalanu103@gmail.com>
2024-04-05 10:14:39 +0530

    using dynamic values

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,43 @@
 import './App.css'
-import {useState} from 'react';
+import { useState } from 'react';
 import Header from './Components/Header';
 import { Components } from './Components/CoreComponents';
-import {Data_Components, EXAMPLES} from './Components/data';
+import { Data_Components, EXAMPLES } from './Components/data';
 import TabButton from './Components/TabButton';
 
 
 
 function App() {
 
-  const [ SelectedContent , SetSelectedContent ] = useState();
+  const [SelectedContent, SetSelectedContent] = useState();
 
-  function handleEvent(selectedvalue){
+  function handleEvent(selectedvalue) {
     SetSelectedContent(selectedvalue);
   }
+
+  let tabcontent = 'Please Select a button';
+
+  if (SelectedContent) {
+    tabcontent = 
+    <div id='content'>
+        <><h2>{EXAMPLES[SelectedContent].title}</h2>
+          <p>{EXAMPLES[SelectedContent].description}</p><pre>
+            <code>
+              {EXAMPLES[SelectedContent].code}
+            </code>
+          </pre></>
+    </div>
+  }
+
 
   return (
     <div>
       <Header></Header>
       <section>
         <ul className='core-conscept'>
-          <Components
-            {...Data_Components[0]} />
-          <Components
-            {...Data_Components[1]} />
-          <Components
-            {...Data_Components[1]} />
+          {Data_Components.map(items => 
+              <Components key={items.Title} {...items}/>
+          )}
         </ul>
       </section>
 
@@ -36,23 +48,12 @@ function App() {
       <section id="examples">
         <h2>Example</h2>
         <menu>
-          <TabButton  label="Components" OnSelect={()=> handleEvent('components')}/>
-          <TabButton  label="JSX" OnSelect={()=> handleEvent('jsx')} /> 
-          <TabButton  label="props" OnSelect={()=> handleEvent('props')}/> 
-          <TabButton  label="state" OnSelect={()=> handleEvent('state')}/>
+          <TabButton label="Components" OnSelect={() => handleEvent('components')} />
+          <TabButton label="JSX" OnSelect={() => handleEvent('jsx')} />
+          <TabButton label="props" OnSelect={() => handleEvent('props')} />
+          <TabButton label="state" OnSelect={() => handleEvent('state')} />
         </menu>
-      <div id='content'>
-            {!SelectedContent ? <p>Plase click a button</p> :  
-
-            <><h2>{EXAMPLES[SelectedContent].title}</h2>
-            <p>{EXAMPLES[SelectedContent].description}</p><pre>
-              <code>
-                {EXAMPLES[SelectedContent].code}
-              </code>
-            </pre></> 
-
-            }
-        </div>
+        {tabcontent}
       </section>
     </div>
   );
